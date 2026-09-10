33c6a84d8fa7fe383f41cfa3418731b27c619379
Fgussx <fgussx@users.noreply.github.com>
2026-09-10 10:42:27 +0700

    Cleanup: Remove setup scripts, unused files, and generated build artifacts

--- create_user.ps1
+++
@@ -1,9 +0,0 @@
-$body = @{
-    username = 'fagus'
-    password = '123456'
-    nama = 'Fagus Admin'
-    id_role = '507f1f77bcf86cd799439011'
-} | ConvertTo-Json
-
-$response = Invoke-WebRequest -Uri 'http://localhost:5000/api/create-user' -Method Post -ContentType 'application/json' -Body $body
-$response.Content
