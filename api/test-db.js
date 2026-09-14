f160148199635945fb52e0f0d351a556d7e14bb3
Fgussx <fgussx@users.noreply.github.com>
2026-09-14 12:39:16 +0700

    fix: Add DNS fix to test-db endpoint

--- api/test-db.js
+++ api/test-db.js
@@ -1,4 +1,7 @@
+import dns from 'dns'
 import mongoose from 'mongoose'
+
+dns.setServers(['8.8.8.8', '1.1.1.1'])
 
 let cached = global.mongoose
 if (!cached) cached = global.mongoose = { conn: null, promise: null }
